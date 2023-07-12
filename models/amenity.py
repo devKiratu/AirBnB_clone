050c975f694816312fa41a0e3d4fcb7ab978e129
Alex Jarabi <jarabialex.gmail.com>
2023-07-12 05:59:11 -0700

    Adding more classes

---
+++ models/amenity.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Models Amenity class
+"""
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """
+    Inherits from BaseModel
+    """
+    name = ""
